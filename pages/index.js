02e29d6e1b7c067e1b8f21e6ebecae7884f13c34
Panthaweekan_Somngam <tatanoobza1234@hotmail.com>
2022-08-11 17:22:28 +0700

    Fix class to className

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
           value={inputNum}
           onChange={(event) => setInputNum(event.target.value)}
         />
-        <button class="btn btn-dark" onClick={() => genUsers()}>
+        <button className="btn btn-dark" onClick={() => genUsers()}>
           Generate
         </button>
       </div>
